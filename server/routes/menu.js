ba7d7f9e90d75db7520d061af6b67bbd3d00c685
Maxim <haisarov-2002@mail.ru>
2025-09-01 20:04:46 +0400

    Добавил редирект utm

--- server/routes/menu.js
+++ server/routes/menu.js
@@ -7,12 +7,15 @@ router.get("/menu", (req, res) => {
   const acceptLang = req.headers["accept-language"] || "";
   const userLang = acceptLang.split(",")[0].split("-")[0];
 
+  // получаем query-строку, если она есть
+  const query = req.url.includes("?") ? req.url.slice(req.url.indexOf("?")) : "";
+
   // если язык поддерживается — редиректим в папку
   if (supportedLangs.includes(userLang) && userLang !== "en") {
-    res.redirect(`/menu/${userLang}/`);
+    res.redirect(`/menu/${userLang}/${query}`);
   } else {
     // по умолчанию английский
-    res.redirect("/menu/");
+    res.redirect(`/menu/${query}`);
   }
 });
 
