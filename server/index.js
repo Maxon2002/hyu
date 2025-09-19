d0283eedcfc898a55e168af384006f99ad8b9900
Maxim <haisarov-2002@mail.ru>
2025-09-19 11:31:28 +0400

    s

--- server/index.js
+++ server/index.js
@@ -13,10 +13,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
-// app.use((req, res, next) => {
-//     console.log("Incoming request:", req.method, req.url);
-//     next();
-// });
+app.use((req, res, next) => {
+    console.log("Incoming request:", req.method, req.url);
+    next();
+});
 // middleware
 app.use(cors());
 app.use(express.json());
