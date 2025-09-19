fdf7949c4c1407dd381b73fa6c35509465b661e6
Maxim <haisarov-2002@mail.ru>
2025-09-19 11:23:37 +0400

    s

--- server/index.js
+++ server/index.js
@@ -12,6 +12,11 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+app.use((req, res, next) => {
+    console.log("Incoming request:", req.method, req.url);
+    next();
+});
 // middleware
 app.use(cors());
 app.use(express.json());
