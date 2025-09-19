66355d43567d7ab5122f92494b667d675989a52d
Maxim <haisarov-2002@mail.ru>
2025-09-19 11:13:48 +0400

    s

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,6 +1,9 @@
 import jwt from "jsonwebtoken";
 
 export function authenticateToken(req, res, next) {
+
+    console.log("authenticateToken called");
+
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
 
@@ -9,6 +12,8 @@ export function authenticateToken(req, res, next) {
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
         if (err) return res.status(403).json({ success: false, message: 'Invalid token' });
         req.userId = user.userId;
+
+        console.log(user.userId)
         next();
     });
 }
