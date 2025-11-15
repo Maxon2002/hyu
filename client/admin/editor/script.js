6bc903d3d8112d933be4873d00eaace101f31b38
Maxim <haisarov-2002@mail.ru>
2025-11-15 11:25:02 +0400

    s

--- client/admin/editor/script.js
+++ client/admin/editor/script.js
@@ -54,8 +54,32 @@ saveBtnsCategory.forEach(saveBtnCategory => {
 exitBtnsCategory.forEach(exitBtnCategory => {
     exitBtnCategory.addEventListener('click', () => {
 
-
-
         exitBtnCategory.closest('.category-block-edit').classList.toggle('active')
     })
+})
+
+
+
+let addCategoryBtn = document.querySelector('.add-category-btn')
+
+addCategoryBtn.addEventListener('click', () => {
+    addCategoryBtn.classList.remove('active')
+    addCategoryBtn.closest('.add-category-container').querySelector('.add-category-block').classList.add('.active')
+})
+
+
+
+let addSaveBtnCategory = document.querySelector('.add-btn-category')
+let cancelBtnCategory = document.querySelector('.cancel-btn-category')
+
+addSaveBtnCategory.addEventListener('click', () => {
+    addSaveBtnCategory.closest('.add-category-block').classList.remove('.active')
+
+    addSaveBtnCategory.closest('.add-category-container').querySelector('.add-category-btn').classList.add('.active')
+})
+
+cancelBtnCategory.addEventListener('click', () => {
+    cancelBtnCategory.closest('.add-category-block').classList.remove('.active')
+
+    cancelBtnCategory.closest('.add-category-container').querySelector('.add-category-btn').classList.add('.active')
 })
